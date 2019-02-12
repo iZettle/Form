8b4119d1fd102fa016f7d0f15a463e17e0515073
Mayur Deshmukh <mayur.deshmukh@izettle.com>
2019-02-12 16:59:27 +0100

    Add test to check TextStyle equality

---
+++ FormTests/TextStyleTests.swift
@@ -0,0 +1,26 @@
+//
+//  TextStyleTests.swift
+//  FormTests
+//
+//  Created by Mayur Deshmukh on 12/02/19.
+//  Copyright © 2019 iZettle. All rights reserved.
+//
+
+import XCTest
+import Form
+
+class TextStyleTests: XCTestCase {
+
+    func testTextStyleEquality() {
+        let textStyle1 = TextStyle(font: .systemFont(ofSize: 14.0), color: .red, alignment: .left, lineBreakMode: .byWordWrapping)
+        let textStyle2 = TextStyle(font: .systemFont(ofSize: 14.0), color: .red, alignment: .left, lineBreakMode: .byWordWrapping)
+        let textStyle3 = TextStyle(font: .systemFont(ofSize: 14.0), color: .red, alignment: .left, lineBreakMode: .byCharWrapping)
+        let textStyle4 = TextStyle(font: .systemFont(ofSize: 15.0), color: .red, alignment: .left, lineBreakMode: .byWordWrapping)
+        let textStyle5 = TextStyle(font: .systemFont(ofSize: 14.0), color: .green, alignment: .left, lineBreakMode: .byWordWrapping)
+
+        XCTAssertEqual(textStyle1, textStyle2)
+        XCTAssertNotEqual(textStyle1, textStyle3)
+        XCTAssertNotEqual(textStyle1, textStyle4)
+        XCTAssertNotEqual(textStyle1, textStyle5)
+    }
+}
