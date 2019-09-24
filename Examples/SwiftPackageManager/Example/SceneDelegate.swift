9bffd44a22ab83d2bbd54e0bf8ed1a50c671f2d9
Robin Enhorn <robin.enhorn@izettle.com>
2019-09-24 17:07:26 +0200

    Make SwiftLint happy

--- Examples/SwiftPackageManager/Example/SceneDelegate.swift
+++ Examples/SwiftPackageManager/Example/SceneDelegate.swift
@@ -12,10 +12,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
-    }
-
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) { }
     func sceneDidDisconnect(_ scene: UIScene) { }
     func sceneDidBecomeActive(_ scene: UIScene) { }
     func sceneWillResignActive(_ scene: UIScene) { }
@@ -23,4 +20,3 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneDidEnterBackground(_ scene: UIScene) { }
 
 }
-
